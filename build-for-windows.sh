0787db2bdef9f0e954583cd0014330eaaf2cc5d9
cblgh <cblgh@cblgh.org>
2025-08-25 16:44:03 +0200

    win: improve build process and set tella logo

--- build-for-windows.sh
+++ build-for-windows.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC="zig cc -target x86_64-windows" CXX="zig cc -target x86_64-windows" go build -tags desktop,production -ldflags "-w -s -H windowsgui" -o build/bin/tella.exe
+CGO_ENABLED=1 GOOS=windows GOARCH=amd64 CC="zig cc -target x86_64-windows" CXX="zig cc -target x86_64-windows" wails build -ldflags "-w -s" -o tella.exe
