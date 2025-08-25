918d09535d989c27488f724b6c343e834df11a4a
cblgh <cblgh@cblgh.org>
2025-08-25 16:16:24 +0200

    fix: format certhash consistent with mobile apps
    this commit formats the certificate fingerprint/hash in a way consistent
    with what is displayed on the mobile apps.
    
    for what it's worth, i suspect that the file `CertificateHash.tsx` is
    left over and unused, but i updated it as well just in case.

--- frontend/src/Components/CertificateHash/CertificateHash.tsx
+++ frontend/src/Components/CertificateHash/CertificateHash.tsx
@@ -6,6 +6,21 @@ interface Props {
     serverRunning: boolean;
 }
 
+function formatHash(hashString: string): string {
+    const input = [];
+    for (let i = 0; i <= hashString.length; i += 4) {
+        // grab groups of 4 characters each
+        let entry = hashString.slice(i, i+4) + " ";
+        // output a newline after four groups of 4
+        if (((i+4) % 16) === 0) {
+            entry += "\n";
+        }
+        input.push(entry);
+    }
+    // trim the last newline and return as a string
+    return input.join("").trim();
+}
+
 export function CertificateHash({ serverRunning }: Props) {
     const [certHash, setCertHash] = useState('');
 
@@ -30,7 +45,7 @@ export function CertificateHash({ serverRunning }: Props) {
     return (
         <div className="status">
             <h3>Certificate Hash:</h3>
-            <code>{certHash.match(/.{1,4}/g)?.join(' ')}</code>
+            <pre><code>{formatHash(certHash)}</code></pre>
         </div>
     );
 }
